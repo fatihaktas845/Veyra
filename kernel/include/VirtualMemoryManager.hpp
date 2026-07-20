edf7184cfcc699acdc483dddca4a4cc9eb1ae251
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-07-20 21:59:05 +0300

    Initial Commit

--- kernel/include/VirtualMemoryManager.hpp
+++ kernel/include/VirtualMemoryManager.hpp
@@ -25,6 +25,8 @@ class VirtualMemoryManager {
 		void mapPage(const PhysicalAddress pa, const VirtualAddress va, const uint64_t flags);
 		void unmapPage(const VirtualAddress va);
 
+		// TODO: Write a physToVirt function!
+
 		VirtualAddress allocPage(const VirtualAddress addr);
 		void freePage(const VirtualAddress va);
 };
