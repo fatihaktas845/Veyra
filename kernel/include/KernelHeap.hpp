c1deb5208b1e417d8c554051e1c6fdc665cb68cc
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-07-20 21:26:05 +0300

    Initial Commit

--- kernel/include/KernelHeap.hpp
+++ kernel/include/KernelHeap.hpp
@@ -4,5 +4,6 @@
 
 namespace KernelHeap {
     constexpr uint64_t KERNEL_HEAP_START = 0xFFFF'9600'0000'0000ULL;
+    constinit uint64_t KERNEL_HEAP_END = KERNEL_HEAP_START;
     constexpr uint64_t KERNEL_HEAP_PML4_INDEX = 300;
 }
