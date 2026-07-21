7a6f24dea1797d17fc37c5bcd46dc526f17c823e
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-07-21 14:35:12 +0300

    Initial Commit

--- kernel/include/KernelHeap.hpp
+++ kernel/include/KernelHeap.hpp
@@ -2,14 +2,10 @@
 
 #include "VirtualMemoryManager.hpp"
 
-#include <stdint.h>
-
-class VirtualMemoryManager;
-
 namespace KernelHeap {
-    constexpr uint64_t KERNEL_HEAP_START = 0xFFFF'9600'0000'0000ULL;
-    constinit uint64_t KERNEL_HEAP_END = KERNEL_HEAP_START;
-    constexpr uint64_t KERNEL_HEAP_PML4_INDEX = 300;
+    uint64_t KERNEL_HEAP_START = 0xFFFF'9600'0000'0000ULL;
+    uint64_t KERNEL_HEAP_END = KERNEL_HEAP_START;
+    uint64_t KERNEL_HEAP_PML4_INDEX = 300;
     VirtualMemoryManager kernelVMM;
 
     struct Block_t {
