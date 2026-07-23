fa3e5f9b3c0ce7e750afbb7a5afd63f2c3815502
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-07-23 15:44:38 +0300

    Initial Commit

--- kernel/src/KernelHeap.cpp
+++ kernel/src/KernelHeap.cpp
@@ -4,10 +4,10 @@
 namespace {
     uint64_t KERNEL_HEAP_START = 0xFFFF'9600'0000'0000ULL;
     uint64_t KERNEL_HEAP_END = KERNEL_HEAP_START;
-    uint64_t KERNEL_HEAP_PML4_INDEX = 300;
+    // uint64_t KERNEL_HEAP_PML4_INDEX = 300;
 
     VirtualMemoryManager kernelVMM; 
-    
+
     KernelHeap::BlockHeader* linkedList = nullptr;
 }
 
