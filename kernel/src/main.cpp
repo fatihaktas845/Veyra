d46c5052b3999c98441e2144849a461a058aa845
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-07-28 15:58:47 +0300

    Initial Commit

--- kernel/src/main.cpp
+++ kernel/src/main.cpp
@@ -16,22 +16,18 @@ extern "C" void kmain() {
 	call_global_constructors();
 
 	KernelHeap::init();
-	apic::init();
-	idt::setInterrupts();
+	// apic::init();
+	// idt::setInterrupts();
 
 	limine_framebuffer* framebuffer = framebuffer_request.response->framebuffers[0];
 
 	volatile uint32_t* framebuffer_address = (volatile uint32_t*)framebuffer->address;
 
-	uint64_t* fbb = new uint64_t;
-	*fbb = reinterpret_cast<uint64_t>(framebuffer_address);
-	delete fbb;
-
 	for (int y = 0; y < 100; y++) {
 		for (int x = 0; x < 100; x++) {
 			int pixel_index = x + (y * framebuffer->pitch / 4);
 
-			framebuffer_address[pixel_index] = 0x00FF0000;
+			framebuffer_address[pixel_index] = 0x00FFFFFF;
 		}
 	}
 
