0864a535ba815a3ef00a6fe9525982a720da05b0
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-07-28 15:18:06 +0300

    Initial Commit

--- kernel/src/apic.cpp
+++ kernel/src/apic.cpp
@@ -4,14 +4,16 @@
 void apic::init() {
     uint64_t apicBase = msr::read(IA32_APIC_BASE_MSR);
 
-    apicBase |= (1 << 11) | (1 << 10);
+    apicBase |= 1ULL << 10;
+    msr::write(IA32_APIC_BASE_MSR, apicBase);
 
+    apicBase |= 1ULL << 11;
     msr::write(IA32_APIC_BASE_MSR, apicBase);
     
-    msr::write(IA32_X2APIC_SIV_MSR, (1 << 8) | 0xFF);
+    msr::write(IA32_X2APIC_SIV_MSR, (1ULL << 8) | 0xFFULL);
 
     // LAPIC Timer
-    msr::write(IA32_X2APIC_DIV_CONF_MSR, 0x3);
-    msr::write(IA32_X2APIC_LVT_TIMER_MSR, (1 << 17) | 0x20);
-    msr::write(IA32_X2APIC_INIT_COUNT_MSR, 20'000'000);
+    msr::write(IA32_X2APIC_DIV_CONF_MSR, 0x3ULL);
+    msr::write(IA32_X2APIC_LVT_TIMER_MSR, (1ULL << 17) | 0x20ULL);
+    msr::write(IA32_X2APIC_INIT_COUNT_MSR, 20'000'000ULL);
 }
