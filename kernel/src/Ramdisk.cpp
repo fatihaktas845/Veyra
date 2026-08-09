dd309ea72fcd7eb0a223f1a7548e5b6b4f94bbe3
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-08-09 13:18:51 +0300

    Initial Commit

--- kernel/src/Ramdisk.cpp
+++ kernel/src/Ramdisk.cpp
@@ -1,5 +1,8 @@
 #include "Ramdisk.hpp"
 
 const void* Ramdisk::readFile(const char* fileName, uint64_t* fileSize_out) {
-    
+    (void)fileName;
+    (void)fileSize_out;
+
+    // Not Sterted Yet!!!
 }
