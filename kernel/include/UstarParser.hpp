632b7f36084e63b7d40ec019f11c7e9b08bb24ca
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-08-09 21:42:07 +0300

    Initial Commit

--- kernel/include/UstarParser.hpp
+++ kernel/include/UstarParser.hpp
@@ -2,27 +2,36 @@
 
 #include <stdint.h>
 
-namespace Ustar {
-    struct FileHeader {
-        char fileName[100];
-        char fileMode[8];
-        char uid[8];
-        char gid[8];
-        char fileSize[12];
-        char mtime[12];
-        char checksum[8];
-        char typeFlag;
-        char linkedFileName[100];
-        char magic[6];
-        char version[2];
-        char uname[32];
-        char gname[32];
-        char major[8];
-        char minor[8];
-        char fileNamePrefix[155];
-        char padding[12];
-    } __attribute__((packed));
+class UstarParser {
+    private:
+        const uint8_t* file = nullptr;
+    
+    public:
+        struct FileHeader {
+            char fileName[100];
+            char fileMode[8];
+            char uid[8];
+            char gid[8];
+            char fileSize[12];
+            char mtime[12];
+            char checksum[8];
+            char typeFlag;
+            char linkedFileName[100];
+            char magic[6];
+            char version[2];
+            char uname[32];
+            char gname[32];
+            char major[8];
+            char minor[8];
+            char fileNamePrefix[155];
+            char padding[12];
+        } __attribute__((packed));
 
-    const void* readFile(void* tarFile, const char* fileName);
-    uint64_t getFileSize(const char* fileName);
-}
+        explicit UstarParser(const uint8_t* ustarFile);
+
+        UstarParser(const UstarParser&) = delete;
+        UstarParser& operator=(const UstarParser&) = delete;
+
+        const uint8_t* readFile(const char* fileName);
+        uint64_t getFileSize(const char* fileName);
+};
--- kernel/src/UstarParser.cpp
+++ kernel/src/UstarParser.cpp
@@ -4,11 +4,15 @@
 #define BLOCK_SIZE 512
 #define ALIGN_UP(value) (((value) + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1))
 
-const void* Ustar::readFile(void* tarFile, const char* fileName) {
+UstarParser::UstarParser(const uint8_t* ustarFile) : file(ustarFile) {
+
+}
+
+const uint8_t* UstarParser::readFile(const char* fileName) {
     if (fileName[0] == '\0')
         return nullptr;
     
-    const uint8_t* currentAddress = reinterpret_cast<const uint8_t*>(tarFile);
+    const uint8_t* currentAddress = this->file;
 
     FileHeader fileEnd[2] = {};
 
@@ -24,12 +28,34 @@ const void* Ustar::readFile(void* tarFile, const char* fileName) {
             continue;
         }
 
-        return reinterpret_cast<const void*>(currentAddress);
+        return currentAddress;
     }
 
     return nullptr;
 }
 
-uint64_t Ustar::getFileSize(const char* fileName) {
+uint64_t UstarParser::getFileSize(const char* fileName) {
+    if (fileName[0] == '\0')
+        return 0;
     
+    const uint8_t* currentAddress = this->file;
+
+    FileHeader fileEnd[2] = {};
+
+    while (Kstd::memcmp(currentAddress, &fileEnd, sizeof(fileEnd)) != 0) {
+        const FileHeader* header = reinterpret_cast<const FileHeader*>(currentAddress);
+        currentAddress += BLOCK_SIZE;
+
+        uint64_t fileSize = Kstd::strToUint64(header->fileSize, 8);
+
+        if (Kstd::strcmp(header->fileName, fileName) != 0) {
+            fileSize = ALIGN_UP(fileSize);
+            currentAddress += fileSize;
+            continue;
+        }
+
+        return fileSize;
+    }
+
+    return 0;
 }
