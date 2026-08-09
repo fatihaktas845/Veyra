a5f8c689599b87269e21ea5602a003e4dc3a6a78
Fatih AKTAŞ <fatihaktas845@gmail.com>
2026-08-09 21:41:17 +0300

    Initial Commit

--- kernel/src/Ramdisk.cpp
+++ kernel/src/Ramdisk.cpp
@@ -6,7 +6,9 @@ __attribute__((used, aligned(8)))
 static volatile struct limine_module_request module_request = {
     .id = LIMINE_MODULE_REQUEST_ID,
     .revision = 0,
-    .response = nullptr
+    .response = nullptr,
+    .internal_module_count = 0,
+    .internal_modules = nullptr
 };
 
 const void* Ramdisk::readFile(const char* fileName, uint64_t* fileSize_out) {
